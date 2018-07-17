27093c8994c18e1588701e9c9b8900c4b853a593
Heather Palmergit config --global user.email heatherpa@zillowgroup.com <palmerheather78@gmail.com>
2018-07-16 17:50:14 -0700

    add 2nd shop in JS

--- app.js
+++ app.js
@@ -49,7 +49,11 @@ var storeHours = document.getElementById('shop1stAndPike');
 storeHours.appendChild(ulEl);
 
 
+
 //SeaTac Airport
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+
 var shopSeaTacAirport = {
     minCustomers: 3,
     maxCustomers: 24,
